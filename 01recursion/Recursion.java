ea81df9d6b6d1bed046c5d32cedb1fdfe1f1eb7b
Catherine Chang <cchang4@stuy.edu>
2016-02-04 22:09:52 -0500

    compiles, name thing works, sqrt has temporary return thing

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -1,3 +1,15 @@
 public class Recursion{
 
+    public String name(){
+	return "Chang, Catherine";
+    }
+
+    public double sqrt(double n){
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	}
+
+	return 10;
+    }
+
 }
