ef8b17902ad990de2026826a135c3929fb552937
Catherine Chang <cchang4@stuy.edu>
2016-02-22 19:00:06 -0500

    i think printsolution works??

--- 03knightstour/KnightBoard.java
+++ 03knightstour/KnightBoard.java
@@ -1,15 +1,41 @@
 public class KnightBoard{
+    private int board[][];
+    
     public KnightBoard(int size){
+	board = new int[size][size];
     }
 
     public KnightBoard(int cols, int rows){
+	board = new int[cols][rows];
     }
 
     public boolean solve(){
+	return true;
     }
 
     public void printSolution(){
+
+	for(int i = 0; i < board.length; i++){
+	    for (int j = 0; j < board[0].length; j++){
+		if (board.length*board[0].length >= 10 && board[i][j] < 10){
+		    System.out.print("_" + board[i][j]+ " ");
+		}else{
+		System.out.print(board[i][j]+ " ");
+		}
+	    }
+	    System.out.println();
+	}
     }
+    /*
+    public static void main(String[]adajsd){
+	KnightBoard k = new KnightBoard(5);
+
+	k.printSolution();
+    }
+
+    */
+
+    
 
 }
 	
