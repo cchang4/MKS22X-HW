5766de2545d61e4c54efb5428d7045b570226026
Catherine Chang <cchang4@stuy.edu>
2016-02-23 22:58:53 -0500

    scanner does something

--- 04USACO/Bronze.java
+++ 04USACO/Bronze.java
@@ -1,11 +1,29 @@
 //file input, print output
 //output, period, last, first
 
-import java.util.*
+import java.util.*;
+import java.io.*;
 
 public class Bronze{
 
+    public static void main(String[] arjsfd){
 
+	File f = new File("makelake.in");
 
+	try{
+	    Scanner sc = new Scanner(f);
 
+	    while(sc.hasNextInt()){
+		int i = sc.nextInt();
+		System.out.println(i);
+	    }
+
+	    sc.close();
+	} catch(FileNotFoundException e){
+	    System.out.println("File not found");
+	}
+    }
+
+        
+	
 }
