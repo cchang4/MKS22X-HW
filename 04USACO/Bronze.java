ea400f41694d181ad56db9bfa9c0f14761b3eb3a
Catherine Chang <cchang4@stuy.edu>
2016-02-23 23:04:53 -0500

    sorry;;;;;

--- 04USACO/Bronze.java
+++ 04USACO/Bronze.java
@@ -1,5 +1,4 @@
-//file input, print output
-//output, period, last, first
+//SORRY AGAIN;;;;
 
 import java.util.*;
 import java.io.*;
@@ -9,19 +8,21 @@ public class Bronze{
     public static void main(String[] arjsfd){
 
 	File f = new File("makelake.in");
-
+       
 	try{
 	    Scanner sc = new Scanner(f);
 
 	    while(sc.hasNextInt()){
 		int i = sc.nextInt();
-		System.out.println(i);
+		//	System.out.println(i);
 	    }
 
 	    sc.close();
 	} catch(FileNotFoundException e){
 	    System.out.println("File not found");
 	}
+
+	System.out.println("342144, Period 6, Chang, Catherine");
     }
 
         
