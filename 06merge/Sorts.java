e8465e1bde4d409266ad770abbc02cd9d9068165
Catherine Chang <cchang4@stuy.edu>
2016-03-08 21:06:04 -0500

    i hate

--- 06merge/Sorts.java
+++ 06merge/Sorts.java
@@ -55,16 +55,46 @@ public class Sorts{
     }
 
     public static void mergesort(int[]data){
-	return 0;
+
+	if(data.length == 1){ printArray(data);}
+
+        mergesort(data, 0, data.length -1);
+	
     }
 
-    public void mergesort(int[]data, int left, int right){
-	return 0;
+    private static void mergesort(int[]data, int left, int right){
+
+	if (right > left){
+	    mergesort(data, left + 1, right);
+	    mergesort(data, left, right - 1);
+	    merge(data, startA, endA, startB, endB);
+	}
+        
     }
 
-    public void merge(){
-	return 0;
+    private static void merge(int[]data, int startA, int endA, int startB, int endB){
+	startA = 0;
+	if ((data.length - 1) % 2 = 0){
+	    endA = (data.length -1 / 2);
+	}else{
+	    endA = (data.length -1 / 2 - 1);
+	}
+	startB = data.length - 1;
+	endB = (data.length -1 / 2 + 1);
+         
     }
+
+    /*   public static void main(String[] args) 
+    {
+        //Unsorted array
+        int[] a = { 2};
+         
+        //Call merge sort
+        mergesort(a);
+
+    }
+
+    */
 
     public static String name(){
 	return "6,Chang,Catherine";  
