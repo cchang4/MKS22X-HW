f101e1b9745c6a4ced1c42b74ceea13f992e118b
Catherine Chang <cchang4@stuy.edu>
2016-03-15 12:29:58 -0400

    rip

--- 08MyLinkedList/MyLinkedList.java
+++ 08MyLinkedList/MyLinkedList.java
@@ -1,10 +1,54 @@
 public class MyLinkedList{
+
+    //Linked lists are made up of nodes, where each node contains a reference to the next node in the list
+
     //variables
     LNode start;
     int size;
 
-    private class LNode{
+    public MyLinkedList(){
 
+    }
+
+    /*
+    //  methods:
+
+  int get(int index)- get the value of the element at the specified index (0 based)
+
+int set(int index,int newValue) - change the value of the element at the specified index to the newValue, return the old value
+
+int size() - return the number of elements in the list
+
+int remove(int index) - remove the element at the specified index, returns the value removed
+
+boolean add(int index, int value) - insert a new elmeent at the specified index, 0 at the front, size() at the end.
+    */
+
+    public boolean add(int value){
+	// - adds to end
+	return false;
+    }
+
+/*
+int indexOf(int value) - returns the index of the 1st occurrence of the value in the linked list, -1 if not found.
+    */
+
+    public String toString() {
+	//- returns a list formatted like: [ v1, v2, v3, ... vn-1, vn ]
+
+	if(start == null){
+	    return "[ ]";
+	}
+
+	return "skdfjdsl";
+    }
+
+    private class LNode{
+	//node???like in vectors??????
+	//ddddd
+
+
+	//points to next one?????
     }
 
 }
