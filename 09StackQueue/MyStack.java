d4aabdc868ed90e44a3cae3dacf7ce7dad06d4b4
Catherine Chang <cchang4@stuy.edu>
2016-03-29 12:22:33 -0400

    aghh i should stop procrastinating

--- 09StackQueue/MyStack.java
+++ 09StackQueue/MyStack.java
@@ -1,9 +1,13 @@
 public class MyStack<T>{
 
-       /**
+    MyLinkedList m = new MyLinkedList<T>();
+
+    /**
      * Adds the given item to the top of the stack.
      */
-    public void push(T item){
+    void push(T item){
+
+	m.add(0, item);
 
     }
 
@@ -11,29 +15,32 @@ public class MyStack<T>{
      * Removes the top item from the stack and returns it.
      * @exception java.util.NoSuchElementException if the queue is empty.
      */
-    //   public T pop(){
+    T pop(){
+	T ans = m.get(0);
+	m.remove(0);
+	return ans;
 
-
+    }
 
     /**
      * Returns the top item from the stack without popping it.
      * @exception java.util.NoSuchElementException if the queue is empty.
      */
-    //    public T peek(){
+    T peek(){
 
-
+    }
 
     /**
      * Returns the number of items currently in the stack.
      */
-    public int size(){
+    int size(){
 	return 0;
     }
 
     /**
      * Returns whether the stack is empty or not.
      */
-    public boolean isEmpty(){
+    boolean isEmpty(){
 	return true;
     }
 }
---
+++ 09StackQueue/ParenDemo.java
@@ -0,0 +1,14 @@
+public class ParenDemo{
+
+    public static boolean isMatching(String s){
+	return true;    
+    }
+
+    public static void main(String[]args){
+	String input = "";
+	if(args.length>0)
+	    input = args[0];
+
+	System.out.println(" ");
+
+}
