973c59265390e87a38242bb4b45e7715235caae3
Catherine Chang <cchang4@stuy.edu>
2016-03-29 22:23:01 -0400

    rip

--- 09StackQueue/ParenDemo.java
+++ 09StackQueue/ParenDemo.java
@@ -1,14 +1,18 @@
 public class ParenDemo{
 
+    //NEED TO FINISH LATER STILL NEED TO CHECK IF THE OTHER THINGS WORK
+
     public static boolean isMatching(String s){
 	return true;    
     }
 
     public static void main(String[]args){
-	String input = "";
-	if(args.length>0)
+	String input = "()()(([[]]))";
+	if(args.length > 0){
 	    input = args[0];
-
-	System.out.println(" ");
-
-}
+	    System.out.println( isMatching(input)); 
+	}else{
+	    System.out.println("Usage:"); 
+	    System.out.println("java ParenDemo \"text\""); 
+	}
+    }
