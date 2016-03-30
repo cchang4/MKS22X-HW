f885c4f5d610209c402f8f1a5a4761cb260112ef
Catherine Chang <cchang4@stuy.edu>
2016-03-29 22:13:45 -0400

    it compiles... i think it works?? IDK HOW TO TEST

--- 09StackQueue/MyStack.java
+++ 09StackQueue/MyStack.java
@@ -1,46 +1,59 @@
+import java.util.*;
 public class MyStack<T>{
 
-    MyLinkedList m = new MyLinkedList<T>();
+     MyLinkedList<T> data;
 
     /**
      * Adds the given item to the top of the stack.
      */
-    void push(T item){
-
-	m.add(0, item);
-
+    public void push(T item){
+       data.add(0, item);
     }
 
     /**
      * Removes the top item from the stack and returns it.
      * @exception java.util.NoSuchElementException if the queue is empty.
      */
-    T pop(){
-	T ans = m.get(0);
-	m.remove(0);
+    public T pop(){
+	if(data.size == 0){ throw new NoSuchElementException();};
+	T ans = data.get(0);
+	data.remove(0);
 	return ans;
-
     }
 
     /**
      * Returns the top item from the stack without popping it.
      * @exception java.util.NoSuchElementException if the queue is empty.
      */
-    T peek(){
-
+    public T peek(){
+	if(data.size == 0){ throw new NoSuchElementException();};
+	return data.get(0);
     }
 
     /**
      * Returns the number of items currently in the stack.
      */
-    int size(){
-	return 0;
+    public int size(){
+	return data.size;
     }
 
     /**
      * Returns whether the stack is empty or not.
      */
-    boolean isEmpty(){
+    public boolean isEmpty(){
+	if(size()> 0){
+	    return false;
+	}
+	
 	return true;
     }
+    /*
+    public static void main(String[]askdh){
+	MyStack<String> s = new MyStack<String>();
+
+	//	s.push("hello");
+
+	System.out.println(s);
+    }
+    */
 }
