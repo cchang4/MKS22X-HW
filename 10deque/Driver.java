641c82b918f80acf240688110ca07b535096d13c
Catherine Chang <cchang4@stuy.edu>
2016-04-06 00:01:03 -0400

    ugh why am i so busy this week

--- 10deque/Driver.java
+++ 10deque/Driver.java
@@ -1,7 +1,12 @@
+import java.util.*;
+
 public class Driver{
     public static void main(String[]aaaaaaaaaaaag){
 	MyDeque<String> d = new MyDeque<String>();
+	ArrayDeque<String> a = new ArrayDeque<String>();
 
 	System.out.println(d);
+	System.out.println(a);
+	
     }
 }
