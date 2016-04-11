d529b5a48fa85e5d8ab508b17907e39e1bc4cd4c
Catherine Chang <cchang4@stuy.edu>
2016-04-10 20:23:06 -0400

    initial files i have no idea what to do lol

---
+++ 11path/Frontier.java
@@ -0,0 +1,24 @@
+public interface Frontier<T>{
+    public void add(T element){
+    }
+    public T next(){
+    }
+    public boolean hasNext(){
+	return true;
+    }
+
+}
+
+public class FrontierStack<T> implements Frontier<T>{
+    /***Make This Work This Weekend!***/
+    /***You can use your classes or built in ones***/
+    /***You can extend another class OR wrap around it***/
+
+}
+
+public class FrontierQueue<T> implements Frontier<T>{
+    /***Make This Work This Weekend!***/
+    /***You can use your classes or built in ones***/
+    /***You can extend another class OR wrap around it***/
+
+}
