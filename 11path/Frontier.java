5d11995f6335dcccaee4e46a3ebea266a0fba0c0
Catherine Chang <cchang4@stuy.edu>
2016-04-12 12:08:18 -0400

    sigh

--- 11path/Frontier.java
+++ 11path/Frontier.java
@@ -1,29 +1,9 @@
 public interface Frontier<T>{
     
-    public void add(T element){
-	
-    }
+    public void add(T element);
     
-    public T next(){
-	
-    }
+    public T next();
     
-    public boolean hasNext(){
-	return true;
-    }
-
-}
-
-public class FrontierStack<T> implements Frontier<T>{
-    /***Make This Work This Weekend!***/
-    /***You can use your classes or built in ones***/
-    /***You can extend another class OR wrap around it***/
-
-}
-
-public class FrontierQueue<T> implements Frontier<T>{
-    /***Make This Work This Weekend!***/
-    /***You can use your classes or built in ones***/
-    /***You can extend another class OR wrap around it***/
+    public boolean hasNext();
 
 }
---
+++ 11path/FrontierQueue.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class FrontierQueue<T> implements Frontier<T>{
+    /***Make This Work This Weekend!***/
+
+    /***You can use your classes or built in ones***/
+
+    /***You can extend another class OR wrap around it***/
+
+    Queue q;
+
+    public void add(T element){
+	q.add(element);
+    }
+
+    public T next(){
+
+    }
+
+    public boolean hasNext(){
+	return true;
+    }
+
+
+}
+
+
