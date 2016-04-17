401c198ce8dd42adcaf2ae7e3bc2b1f147573a93
Catherine Chang <cchang4@stuy.edu>
2016-04-16 21:22:54 -0400

    i hate everything where did my colored pencils go

---
+++ 12tree/TreeNode.java
@@ -0,0 +1,12 @@
+TreeNode<T>{
+  //what goes here?
+    /*
+-getData() (maybe) setData... but making a new node to replace it works too.  
+-getLeft()  -setLeft()   
+-getRight() -setRight()
+//If the tree is tally based rather than storing duplicates:
+-getCount()      -setCount()     
+    */
+
+    
+}
