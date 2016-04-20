95d6afe126ff901ff5f3e25d357ffc0f490b3ecf
Catherine Chang <cchang4@stuy.edu>
2016-04-20 12:25:32 -0400

    uh

--- 13bstree/BSTree.java
+++ 13bstree/BSTree.java
@@ -1,3 +1,20 @@
 public class BSTree<T extends Comparable<T>>{
+    Node root;
+
+    public class Node{
+    Node left, right;
+    T data;
+
+	//get set data left right
+
+    }
+
+    /*
+public void add(T value)
+public String toString() ***
+public boolean contains(T value)
+public int getHeight()  //this can be linear for now.
+    */
+    
 
 }
