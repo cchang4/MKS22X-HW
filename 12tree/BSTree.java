6c84c1ba12192033672f28c4686fa5f873b7a04f
Catherine Chang <cchang4@stuy.edu>
2016-04-21 12:15:35 -0400

    Node set/get methods added i think

--- 12tree/BSTree.java
+++ 12tree/BSTree.java
@@ -1,44 +1,55 @@
-public class BSTree<T extends comparable<T>>{
+public class BSTree<T extends Comparable<T>>{
     private class Node{
- T data;
- Node left;
- Node right;
- // set/get: data/left/right
+	T data;
+	Node left;
+	Node right;
+	// set/get: data/left/right
 
+	public T getData(){ return data;}
+	
+	public Node getLeft(){ return left;}
 
- //real methods here
- public int height(){ 
-    return 0;
- }
- public void add(T value){
- }
- public String toString(){
-    return "";
- }
- public boolean contains(T value){
-    return false;
- }
+	public Node getRight(){ return right;}
+
+	public void setData(T d){ data = d;}
+
+	public void setLeft(Node l){ left = l;}
+	
+	public void setRight(Node r){ right = r;}
+
+	//real methods here
+	public int height(){ 
+	    return 0;
+	}
+	public void add(T value){
+	}
+	public String toString(){
+	    return "gods";
+	}
+	public boolean contains(T value){
+	    return false;
+	}
     
     }
 
     private Node root;
 
     //OUTER methods here are wrapper methods for the root
-    public getHeight(){
- //call the root's methods
- //check for empty first!
- return root.height();
+    public int getHeight(){
+	//call the root's methods
+	//check for empty first!
+	return root.height();
     }
 
     public void add(T value){
- //check for empty before you do things with root.
+	//check for empty before you do things with root.
     }
     public String toString(){
- //check for empty before you do things with root.
- return "";
+	//check for empty before you do things with root.
+	return "";
     }
     public boolean contains(T value){
- //check for empty before you do things with root.
- return false;
+	//check for empty before you do things with root.
+	return false;
     }
 }
